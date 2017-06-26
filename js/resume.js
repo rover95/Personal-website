bd409235fa2064228ce54bc3b33a4dbd8b893ab9
rover95 <rover95rx@gmail.com>
2017-06-27 02:22:39 +0800

    新建resume页面，修改相关链接

---
+++ js/resume.js
@@ -0,0 +1,13 @@
+//首页背景图自适应
+if(window.screen.height*16<9*window.screen.width){
+	R('.p1').css('background-size','100% auto')
+}
+
+//相应菜单按钮
+R('.min-nav').toggle(function(){
+	R('.head-menu').css('display','block');
+	R('.min-nav').first().src='../icon/menu.png';
+},function(){
+	R('.head-menu').css('display','none');
+	R('.min-nav').first().src='../icon/menu-h.png';
+})
